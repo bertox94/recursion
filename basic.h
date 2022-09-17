6fe95c63dd8dd716109336d577ecf0381316871d
bertox94 <10519773@polimi.it>
2022-09-17 20:16:53 +0200

    OK

--- basic.h
+++ basic.h
@@ -78,9 +78,10 @@ int how_many(Node<T> *tree) {
 
 //A tree consists of a root, and zero or more trees T1, T2, . . ., Tk
 //An empty tree doesn't exist. At least root must be.
-//Just think that  leaf is just a root without children
+//When thinking what to return and stuff, just think that a leaf is just a root without children
 template<typename T>
 T max_value(Node<T> *tree) {
+    int m_val_among_the_children;
     int m_val;
     if (tree->children.empty()) {
         //empty statement for the children
@@ -89,39 +90,42 @@ T max_value(Node<T> *tree) {
         std::list<int> maxes;
         for (auto &child: tree->children)
             maxes.push_back(max_value(child));
-        m_val = max_utils(maxes); // for the children
-        m_val = std::max(m_val, *tree->item); // for the father
+        m_val_among_the_children = max_utils(maxes); // for the children
+        m_val = std::max(m_val_among_the_children, *tree->item); // for the father
     }
     return m_val;
 }
 
 template<typename T>
 T min_value(Node<T> *tree) {
+    int m_val_among_the_children;
     int m_val;
     if (tree->children.empty()) {
-        m_val = *tree->item;
+        //empty statement for the children
+        m_val = *tree->item; // for the father
     } else {
         std::list<int> minima;
         for (auto &child: tree->children)
             minima.push_back(min_value(child));
-        minima.push_back(*tree->item);
-        m_val = min_utils(minima);
+        m_val_among_the_children = min_utils(minima); // for the children
+        m_val = std::min(m_val_among_the_children, *tree->item); // for the father
     }
-
     return m_val;
 }
 
 template<typename T>
 T how_many_like_this(Node<T> *tree, T like) {
+    int num_of_nodes_of_the_children;
     int num_of_nodes;
     if (tree->children.empty()) {
-        num_of_nodes = 0;
+        num_of_nodes_of_the_children = 0;
     } else {
         std::list<int> nodes;
         for (auto &child: tree->children)
             nodes.push_back(how_many_like_this(child, like));
-        num_of_nodes = sum_utils(nodes);
+        num_of_nodes_of_the_children = sum_utils(nodes);
     }
+    num_of_nodes = num_of_nodes_of_the_children;
     if (*tree->item == like)
         num_of_nodes++;
     return num_of_nodes;
@@ -131,7 +135,8 @@ template<typename T>
 list<T> list_nodes(Node<T> *tree) {
     std::list<int> nodes;
     if (tree->children.empty()) {
-        // no need to do anything for the children, for the father will be done later once for this two cases
+        // for the children, the list is already empty, no need to do anything
+        // for the father will be done later once for this two cases
     } else {
         std::list<int> children_nodes;
         for (auto &child: tree->children)
@@ -148,14 +153,18 @@ std::pair<int, int> number_of_nodes_at_depth_one(Node<T> *tree, int depth) {
 }
 
 //define the specified_height of a node as the maximum of the heights of its children +1
+//here you can see that the dealing with the father variable and the computation of the returned variable are split
+// probably because we return a pair of two values
+// height is computated in each branch, nodes once outside the branches
 template<typename T>
 std::pair<int, int> number_of_nodes_at_specific_height(Node<T> *tree, int specified_height) {
-    std::list<int> nodes;
+    std::list<int> nodes_from_the_children;
     std::list<int> heights_of_the_children;
     //the following are left unspecified for now because we don't know what value is it now
     int num_of_nodes_of_the_children;
     int current_num_of_nodes;
     int height_of_the_children;
+    int max_height_among_the_children;
     int current_height;
 
     if (tree->children.empty()) {
@@ -165,16 +174,16 @@ std::pair<int, int> number_of_nodes_at_specific_height(Node<T> *tree, int specif
         for (auto &child: tree->children) {
             auto ret = number_of_nodes_at_specific_height(child, specified_height);
             heights_of_the_children.push_back(ret.first);
-            nodes.push_back(ret.second);
+            nodes_from_the_children.push_back(ret.second);
         }
-        current_height = max_utils(heights_of_the_children) + 1;
-        num_of_nodes_of_the_children = sum_utils(nodes);
+        max_height_among_the_children = max_utils(heights_of_the_children);
+        current_height = max_height_among_the_children + 1;
+        num_of_nodes_of_the_children = sum_utils(nodes_from_the_children);
     }
 
+    current_num_of_nodes = num_of_nodes_of_the_children;
     if (current_height == specified_height)
         current_num_of_nodes = num_of_nodes_of_the_children + 1;
-    else
-        current_num_of_nodes = num_of_nodes_of_the_children;
 
     return {current_height, current_num_of_nodes};
 }
