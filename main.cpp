9f0517034b0f9d82c0de2b27898bc49dff55f654
bertox94 <10519773@polimi.it>
2022-09-18 21:13:39 +0200

    OK

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main() {
     cout << "-------" << endl;
     for (auto i = 0; i <= 25; i++) {
         string str = string("NFSNC (") + to_string(i) + "):";
-        //cout << setw(13) << left << str << number_of_fathers_with_specified_number_of_children(tree, i) << endl;
+        cout << setw(13) << left << str << number_of_fathers_with_specified_number_of_children(tree, i) << endl;
     }
     cout << "DDFSC:      " << depth_of_the_deepest_father_with_single_child(tree).second << endl;
     auto tuple = number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(tree, 1);
