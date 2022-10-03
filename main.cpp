ac439ada197695e20fe0f4a5c0510f81e126c6bb
bertox94 <10519773@polimi.it>
2022-10-03 16:51:42 +0200

    OK

--- main.cpp
+++ main.cpp
@@ -34,99 +34,99 @@ void testTree() {
     cout << "-------" << endl;
     print(root);
 
-     //for (auto i = 0; i <= max_depth(root, -1) + 25; i++) {
-     //    string str = string("B degree (") + to_string(i) + "):";
- //
-     //    cout << setw(13) << left << str
-     //         << max_num_of_direct_children_at_depth(root, RightAttr<int>(-1, i)).num -
-     //            std::get<1>(min_num_of_direct_children_at_depth(root, -1, i))
-     //         << endl;//", " << res2 << endl;
-     //}
+    //for (auto i = 0; i <= max_depth(root, -1) + 25; i++) {
+    //    string str = string("B degree (") + to_string(i) + "):";
+    //
+    //    cout << setw(13) << left << str
+    //         << max_num_of_direct_children_at_depth(root, RightAttr<int>(-1, i)).num -
+    //            std::get<1>(min_num_of_direct_children_at_depth(root, -1, i))
+    //         << endl;//", " << res2 << endl;
+    //}
 
-     //cout << "Balance Factor: " << (double) number_of_fathers_with_no_child(root) / how_many(root) << endl;
-     cout << "-------" << endl;
-     for (auto i = minval; i <= minval + 25; i++) {
-         string str = string("HMLT (") + to_string(i) + "):";
-         auto res1 = how_many_like_this(root, i);
-         //auto res2 = how_many_like_this_variant(root, i, 0);
-         //if (res1 != res2)
-         cout << setw(13) << left << str << res1 << endl;//", " << res2 << endl;
-     }
-     cout << "-------" << endl;
-     for (auto i = 0; i <= 25; i++) {
-         string str = string("NNSH (") + to_string(i) + "):";
-         cout << setw(13) << left << str << number_of_nodes_at_specific_height(root, i).second << endl;
-     }
-     cout << "-------" << endl;
-     for (auto i = 0; i <= 25; i++) {
-         string str = string("NFSNC (") + to_string(i) + "):";
-         cout << setw(13) << left << str << number_of_fathers_with_specified_number_of_children(root, i) << endl;
-     }
-     cout << "-------" << endl;
-     for (auto i = 0; i <= 25; i++) {
-         string str = string("MaxNOCD (") + to_string(i) + "):";
-         auto L = max_num_of_direct_children_at_depth(root, RightAttr<int>(_depth(-1), _targetdepth(i)));
-         cout << setw(13) << left << str << (L.valid ? to_string(L.num) : "-") << endl;
-     }
-     cout << "-------" << endl;
-     for (auto i = 0; i <= 25; i++) {
-         string str = string("MinNOCD (") + to_string(i) + "):";
-         auto tuple = min_num_of_direct_children_at_depth(root, -1, i);
-         cout << setw(13) << left << str << (get<0>(tuple) ? to_string(get<1>(tuple)) : "-") << endl;
-     }
+    //cout << "Balance Factor: " << (double) number_of_fathers_with_no_child(root) / how_many(root) << endl;
+    cout << "-------" << endl;
+    for (auto i = minval; i <= minval + 25; i++) {
+        string str = string("HMLT (") + to_string(i) + "):";
+        auto res1 = how_many_like_this(root, i);
+        //auto res2 = how_many_like_this_variant(root, i, 0);
+        //if (res1 != res2)
+        cout << setw(13) << left << str << res1 << endl;//", " << res2 << endl;
+    }
+    cout << "-------" << endl;
+    for (auto i = 0; i <= 25; i++) {
+        string str = string("NNSH (") + to_string(i) + "):";
+        cout << setw(13) << left << str << number_of_nodes_at_specific_height(root, i).second << endl;
+    }
+    cout << "-------" << endl;
+    for (auto i = 0; i <= 25; i++) {
+        string str = string("NFSNC (") + to_string(i) + "):";
+        cout << setw(13) << left << str << number_of_fathers_with_specified_number_of_children(root, i) << endl;
+    }
+    cout << "-------" << endl;
+    for (auto i = 0; i <= 25; i++) {
+        string str = string("MaxNOCD (") + to_string(i) + "):";
+        auto L = max_num_of_direct_children_at_depth(root, RightAttr<int>(_depth(-1), _targetdepth(i)));
+        cout << setw(13) << left << str << (L.valid ? to_string(L.num) : "-") << endl;
+    }
+    cout << "-------" << endl;
+    for (auto i = 0; i <= 25; i++) {
+        string str = string("MinNOCD (") + to_string(i) + "):";
+        auto tuple = min_num_of_direct_children_at_depth(root, -1, i);
+        cout << setw(13) << left << str << (get<0>(tuple) ? to_string(get<1>(tuple)) : "-") << endl;
+    }
 
-     cout << "-------" << endl;
-     auto lll = list_nodes_II(root);
-     std::vector<LeftAttr<int>> ll{lll.compositeList.begin(), lll.compositeList.end()};
-     std::sort(ll.begin(), ll.end(), [](auto &left, auto &right) { return left.value < right.value; });
-     int num = 0;
-     for (auto &el: ll) {
-         string str = string("Report (") + to_string(el.value) + "):";
-         cout << setw(13) << left << str << to_string(el.num) << endl;
-         if (num > 25) {
-             break;
-         }
-         num++;
-     }
+    cout << "-------" << endl;
+    auto lll = list_nodes_II(root);
+    std::vector<LeftAttr<int>> ll{lll.compositeList.begin(), lll.compositeList.end()};
+    std::sort(ll.begin(), ll.end(), [](auto &left, auto &right) { return left.value < right.value; });
+    int num = 0;
+    for (auto &el: ll) {
+        string str = string("Report (") + to_string(el.value) + "):";
+        cout << setw(13) << left << str << to_string(el.num) << endl;
+        if (num > 25) {
+            break;
+        }
+        num++;
+    }
 
-     cout << "-------" << endl;
-     auto llll = duplicates(root);
-     std::vector<LeftAttr<int>> duplicatess{llll.compositeList.begin(), llll.compositeList.end()};
-     std::vector<int> singletonss{llll.simpleList.begin(), llll.simpleList.end()};
-     std::sort(duplicatess.begin(), duplicatess.end(),
-               [](auto &left, auto &right) { return left.value < right.value; });
-     std::sort(singletonss.begin(), singletonss.end(), [](auto &left, auto &right) { return left < right; });
-     std::cout << "Elements that appear once: " << std::endl;
-     num = 0;
-     for (auto &el: singletonss) {
-         cout << to_string(el) << endl;
-         if (num > 25) {
-             break;
-         }
-         num++;
-     }
+    cout << "-------" << endl;
+    auto llll = duplicates(root);
+    std::vector<LeftAttr<int>> duplicatess{llll.compositeList.begin(), llll.compositeList.end()};
+    std::vector<int> singletonss{llll.simpleList.begin(), llll.simpleList.end()};
+    std::sort(duplicatess.begin(), duplicatess.end(),
+              [](auto &left, auto &right) { return left.value < right.value; });
+    std::sort(singletonss.begin(), singletonss.end(), [](auto &left, auto &right) { return left < right; });
+    std::cout << "Elements that appear once: " << std::endl;
+    num = 0;
+    for (auto &el: singletonss) {
+        cout << to_string(el) << endl;
+        if (num > 25) {
+            break;
+        }
+        num++;
+    }
 
-     cout << "-------" << endl;
-     std::cout << "Elements that appear more then once: " << std::endl;
-     num = 0;
-     for (auto &el: duplicatess) {
-         cout << to_string(el.value) << ", " << to_string(el.num) << endl;
-         if (num > 25) {
-             break;
-         }
-         num++;
-     }
-     cout << "-------" << endl;
+    cout << "-------" << endl;
+    std::cout << "Elements that appear more then once: " << std::endl;
+    num = 0;
+    for (auto &el: duplicatess) {
+        cout << to_string(el.value) << ", " << to_string(el.num) << endl;
+        if (num > 25) {
+            break;
+        }
+        num++;
+    }
+    cout << "-------" << endl;
 
-     cout << "DDFSC:      " << depth_of_the_deepest_father_with_single_child(root, -1).second << endl;
-     cout << "MNOC:      " << max_num_of_direct_children(root) << endl;
-     auto tuple = number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(root, -1, 1);
-     cout << "NFWSCSMD:   " << get<1>(tuple) << ", " << get<2>(tuple) << endl;
+    cout << "DDFSC:      " << depth_of_the_deepest_father_with_single_child(root, -1).second << endl;
+    cout << "MNOC:      " << max_num_of_direct_children(root) << endl;
+    auto tuple = number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(root, -1, 1);
+    cout << "NFWSCSMD:   " << get<1>(tuple) << ", " << get<2>(tuple) << endl;
 
     //auto ll = list_nodes(root);
     //cout << "List:       \n" << print(ll) << endl;
 
-    delete root;
+    deletee(root);
 }
 
 int main() {
