03034965dca1773979a385ff6d7dc9f0236d2aa7
bertox94 <10519773@polimi.it>
2022-10-03 13:29:57 +0200

    OK

--- easy/basic.h
+++ easy/basic.h
@@ -199,14 +199,14 @@ int how_many_like_this(Node<T> *node, T Rfather) {
 //here you see how thr right param is correctly used
 // so right is used in the curr node as it is passed from the parent, then changed only to be passed to its child
 template<typename T>
-void print(Node<T> *node, int depth = 0) {
+void print(Node<T> *node, int spacing = 0) {
     if (node->has_children()) {
-        std::cout << std::string(3 * depth, ' ') << (depth > 0 ? "+ " : "") << *node->item << ":" << std::endl;
+        std::cout << std::string(spacing, ' ') << (spacing > 0 ? "+ " : "") << *node->item << ":" << std::endl;
         for (auto &child: node->children) {
-            print(child, depth + 1);
+            print(child, spacing + 3);
         }
     } else {
-        std::cout << std::string(3 * depth, ' ') << "+ " << *node->item << std::endl;
+        std::cout << std::string(spacing, ' ') << "+ " << *node->item << std::endl;
     }
 
 }
