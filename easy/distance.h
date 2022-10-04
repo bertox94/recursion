2da77589950734e481256e4103d613a2a6400c18
Alberto Tiraboschi <halibert94@gmail.com>
2022-10-04 10:06:55 +0200

    OK

---
+++ easy/distance.h
@@ -0,0 +1,22 @@
+//
+// Created by Halib on 03.10.2022.
+//
+
+#ifndef RECURSION_DISTANCE_H
+#define RECURSION_DISTANCE_H
+
+#include <tuple>
+#include "../Node.h"
+
+//it would be nice to know those two leafs
+template<typename T>
+std::tuple<int, int, int> max_distance(Node<T> *node, int depth) {
+    std::vector<int> max_depths;
+    std::vector<int> max_distances;
+    std::vector<int> deepest_common_father;
+    if (node->has_children()) {
+
+    }
+}
+
+#endif //RECURSION_DISTANCE_H
--- easy/parsing.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Halib on 03.10.2022.
-//
-
-#ifndef RECURSION_PARSING_H
-#define RECURSION_PARSING_H
-
-#endif //RECURSION_PARSING_H
