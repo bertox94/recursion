e9df4e2e7e361c5e0c509471649a5ea8921c3d89
bertox94 <10519773@polimi.it>
2022-12-02 16:42:47 +0100

    OK

--- easy/fathers.h
+++ easy/fathers.h
@@ -155,39 +155,42 @@ depth_of_the_deepest_father_with_specified_number_of_children(Node<T> *node, int
 }
 
 template<typename T>
-std::tuple<bool, int, int> //do the same also with min, so that you test all properties (i.e. testproperty on children branch)
+std::tuple<bool, int, int>
 number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(Node<T> *node, int current_depth,
                                                                                    int children) {
-    std::list<std::tuple<bool, int, int>> ll;
-    for (auto &child: node->children) {
-        auto ret = number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(child,
-                                                                                                      current_depth + 1,
-                                                                                                      children);
-        ll.emplace_back(ret);
+
+    if (node->has_children()) {
+        std::vector<std::tuple<int, int>> ll;
+        for (auto &child: node->children) {
+            auto [valid, max_depth, count] = number_of_fathers_with_specified_number_of_children_at_maximal_and_thus_same_depth(
+                    child,
+                    current_depth + 1, children);
+            if (valid)
+                ll.emplace_back(max_depth, count);
+        }
+        if (!ll.empty()) {
+            auto max_depth = std::get<0>(*max_element(ll.begin(), ll.end(),
+                                                      [](auto a, auto b) { return std::get<0>(a) < std::get<0>(b); }));
+            auto count = accumulate(ll.begin(), ll.end(), 0, [&](auto acc, auto b) {
+                return acc += std::get<0>(b) == max_depth ? std::get<1>(b) : 0;
+            });
+            return {true, max_depth, count};
+        } else {
+            //all children has no father with specified num. of child
+            if (node->children.size() == children) {
+                return {true, current_depth, 1};
+            } else {
+                //nope, not even curr
+                return {false, std::rand(), std::rand()};
+            }
+        }
+    } else {
+        if (node->children.size() == children) {
+            return {true, current_depth, 1};
+        } else {
+            return {false, std::rand(), std::rand()};
+        }
     }
-
-    if (node->children.size() == children)
-        ll.emplace_back(true, current_depth, 1);
-
-    bool found;
-    int depth;
-    int number;
-
-    ll.erase(std::remove_if(ll.begin(), ll.end(), [](auto &el) { return !std::get<0>(el); }), ll.end());
-    if (!ll.empty()) {
-        found = true;
-        depth = std::get<1>(*max_element(ll.begin(), ll.end(),
-                                         [](auto &l, auto &r) { return get<1>(l) < get<1>(r); }));
-        number = accumulate(
-                ll.begin(), ll.end(), 0,
-                [&](auto acc, auto tpl) {
-                    return std::get<1>(tpl) == depth ? acc + std::get<2>(tpl) : acc;
-                }
-        );
-    } else
-        found = false;
-
-    return {found, depth, number};
 }
 
 #endif //RECURSION_FATHERS_H
